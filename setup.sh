ab8a674b1eb1921aa061305c16c7735a5f9458da
Zayxc <141384673+Zayxc@users.noreply.github.com>
2023-08-08 09:37:25 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ BURIQ () {
 }
 # https://raw.githubusercontent.com/Zayxc/supreme/aio/permission/ip 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/Zayxc/supreme/aio/permission/ip | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/Zayxc/VPN/aio/permission/ip | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
